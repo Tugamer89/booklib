3a89aecbda4ed3e0bb1bcefcbd4380a14ee542c7
Tuga <61603718+Tugamer89@users.noreply.github.com>
2025-09-04 01:06:36 +0200

    Improved jobs with multiple workers

--- utils/starter.py
+++ utils/starter.py
@@ -1,3 +1,4 @@
+import os
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from contextlib import asynccontextmanager
@@ -13,31 +14,33 @@ scheduler = AsyncIOScheduler()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
-    
-    # Keepalive HTTP
-    if settings.keepalive_url:
+
+    if os.getpid() == os.getppid() or settings.DEBUG:
+        # Keepalive HTTP
+        if settings.keepalive_url:
+            scheduler.add_job(
+                keepalive_job,
+                trigger=CronTrigger.from_crontab(settings.keepalive_cron),
+                id="keepalive_http",
+                replace_existing=True
+            )
+            logger.info(f"[KEEPALIVE] HTTP job started with cron: {settings.keepalive_cron}")
+
+        # Keepalive DB
         scheduler.add_job(
-            keepalive_job,
-            trigger=CronTrigger.from_crontab(settings.keepalive_cron),
-            id="keepalive_http",
-            replace_existing=True,
+            keepalive_db_job,
+            trigger=CronTrigger.from_crontab(settings.keepalive_db_cron),
+            id="keepalive_db",
+            replace_existing=True
         )
-        logger.info(f"[KEEPALIVE] HTTP job started with cron: {settings.keepalive_cron}")
-        
-    # Keepalive DB
-    scheduler.add_job(
-        keepalive_db_job,
-        trigger=CronTrigger.from_crontab(settings.keepalive_db_cron),
-        id="keepalive_db",
-        replace_existing=True,
-    )
-    logger.info(f"[KEEPALIVE] DB job started with cron: {settings.keepalive_db_cron}")
-    
-    if not scheduler.running:
-        scheduler.start()
-    
+        logger.info(f"[KEEPALIVE] DB job started with cron: {settings.keepalive_db_cron}")
+
+        if not scheduler.running:
+            scheduler.start()
+
     yield
-    
+
+    # Shutdown
     if scheduler.running:
         scheduler.shutdown(wait=False)
         logger.info(f"[KEEPALIVE] Scheduler shutdown.")
